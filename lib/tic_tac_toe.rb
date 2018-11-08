3f30c11bb60d944650aa1d644ccdaec607da5370
Joe Quattrone <joequattrone5@gmail.com>
2018-11-08 16:22:32 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -32,10 +32,8 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-   if !position_taken?(board, index) && index.between?(0,9)
-    true
-  else false
-  end
+   !position_taken?(board, index) && index.between?(0,9)
+   
 end
 
 
